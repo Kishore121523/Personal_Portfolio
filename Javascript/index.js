5764db46afbba1a31fc554038c14d09ba7a276cd
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2022-01-15 22:24:51 +0530

    Add files via upload

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,208 +1,164 @@
-//Intersection Video
-const header = document.querySelector("header");
+//Navbar display function
+const nav = document.querySelector("nav");
+const up = document.getElementById("go-up")
 const sectionOne = document.querySelector("#home-section");
 const sectionOneOptions = {
-  rootMargin: "-50px 0px 0px 0px",
+    rootMargin: "-20px 0px 0px 0px",
 };
-const sectionOneObserver = new IntersectionObserver(function (
-  entries,
-  sectionOneObserver
-) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      header.classList.remove("nav-open");
-      header.classList.add("nav-close");
-    } else {
-      header.classList.remove("nav-close");
-      header.classList.add("nav-open");
-    }
-  });
-},
-sectionOneOptions);
-
+const sectionOneObserver = new IntersectionObserver(function(
+        entries,
+        sectionOneObserver
+    ) {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                nav.classList.add("nav-close");
+                up.className = "text-icon-up-close";
+            } else {
+                nav.classList.remove("nav-close");
+                up.classList.remove("text-icon-up-close");
+                up.className = "text-icon-up-open";
+            }
+        });
+    },
+    sectionOneOptions);
 sectionOneObserver.observe(sectionOne);
-
-// // Video animation
-// const intro = document.querySelector(".intro");
-// const video = intro.querySelector("video");
-// const text1 = intro.querySelector(".note1");
-// const text2 = intro.querySelector(".note2");
-
-// //End section
-// const end = document.querySelector("#about-section");
-
-// //ScrollMagic
-// const controller = new ScrollMagic.Controller();
-// const controller1 = new ScrollMagic.Controller();
-
-// // Scenes
-// let scene = new ScrollMagic.Scene({
-//   duration: 6800,
-//   triggerElement: intro,
-//   triggerHook: 0,
-// })
-//   .setPin(intro)
-//   .addTo(controller);
-
-// // TextAnimation
-// let textanim1 = TweenMax.fromTo(text1, 1, { opacity: 1 }, { opacity: 0 });
-// let textanim2 = TweenMax.fromTo(text2, 1, { opacity: 0 }, { opacity: 1 });
-// let textanim3 = TweenMax.fromTo(text2, 1, { opacity: 1 }, { opacity: 0 });
-
-// let scene1 = new ScrollMagic.Scene({
-//   duration: 2500,
-//   offset: 1000,
-//   triggerElement: intro,
-//   triggerHook: 0,
-// })
-//   .setTween(textanim1)
-//   .addTo(controller1);
-
-// let scene2 = new ScrollMagic.Scene({
-//   duration: 2000,
-//   offset: 3700,
-//   triggerElement: intro,
-//   triggerHook: 0,
-// })
-//   .setTween(textanim2)
-//   .addTo(controller1);
-
-// let scene3 = new ScrollMagic.Scene({
-//   duration: 4700,
-//   offset: 4700,
-//   triggerElement: intro,
-//   triggerHook: 0.1,
-// })
-//   .setTween(textanim3)
-//   .addTo(controller1);
-
-// // controller.remove([scene1]);
-// // Animation
-// let accelamount = 0.1;
-// let scrollposition = 0;
-// let delay = 0;
-
-// scene.on("update", (e) => {
-//   scrollposition = e.scrollPos / 1000;
-// });
-
-// setInterval(() => {
-//   delay += (scrollposition - delay) * accelamount;
-//   video.currentTime = delay;
-// }, 70);
 
 // Mail Send Function
 function sendMail() {
-  Email.send({
-    SecureToken: "b9409e6f-9fd5-47cd-b3ee-6f7c0ad00783",
-    To: "contactkishorework@gmail.com",
-    From: `${document.getElementById("email").value}`,
-    Subject: `Sender Name - ${document.getElementById("name").value}`,
-    Body: `${document.getElementById("message").value}`,
-  });
-  document.getElementById("name").value = "";
-  document.getElementById("email").value = "";
-  document.getElementById("message").value = "";
-  // .then((message) => alert(message));
+    Email.send({
+        SecureToken: "b9409e6f-9fd5-47cd-b3ee-6f7c0ad00783",
+        To: "contactkishorework@gmail.com",
+        From: `${document.getElementById("email").value}`,
+        Subject: `Sender Name - ${document.getElementById("name").value}`,
+        Body: `${document.getElementById("message").value}`,
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    // .then((message) => alert(message));
 }
 
 // TypeWriter Animation
-var TxtType = function (el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = "";
-  this.tick();
-  this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function () {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
-
-  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
-
-  var that = this;
-  var delta = 200 - Math.random() * 100;
-
-  if (this.isDeleting) {
-    delta /= 2;
-  }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === "") {
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = "";
+    this.tick();
     this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
-
-  setTimeout(function () {
-    that.tick();
-  }, delta);
 };
 
-window.onload = function () {
-  var elements = document.getElementsByClassName("typewrite");
-  for (var i = 0; i < elements.length; i++) {
-    var toRotate = elements[i].getAttribute("data-type");
-    var period = elements[i].getAttribute("data-period");
-    if (toRotate) {
-      new TxtType(elements[i], JSON.parse(toRotate), period);
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-  document.body.appendChild(css);
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) {
+        delta /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName("typewrite");
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute("data-type");
+        var period = elements[i].getAttribute("data-period");
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
 };
 
 function myFunction() {
-  document.getElementById("myForm").reset();
+    document.getElementById("myForm").reset();
 }
+
 // animate on scroll
 AOS.init();
 
 //cursor animation
 var cursor = document.querySelector(".cursor");
 var cursor2 = document.querySelector(".cursor2");
-document.addEventListener("mousemove", function (e) {
-  cursor.style.cssText = cursor2.style.cssText =
-    "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+document.addEventListener("mousemove", function(e) {
+    cursor.style.cssText = cursor2.style.cssText =
+        "left: " + e.clientX + "px; top: " + e.clientY + "px;";
 });
 
 //init scrollspy
 $("body").scrollspy({ target: "#main-nav" });
 
 // Add smooth scrolling
-$("#main-nav a").on("click", function (e) {
-  // Check for a hash value
-  if (this.hash !== "") {
-    // Prevent default behavior
-    e.preventDefault();
+$("#main-nav a").on("click", function(e) {
+    // Check for a hash value
+    if (this.hash !== "") {
+        // Prevent default behavior
+        e.preventDefault();
 
-    // Store hash
-    const hash = this.hash;
+        // Store hash
+        const hash = this.hash;
 
-    // Animate smooth scroll
-    $("html, body").animate(
-      {
-        scrollTop: $(hash).offset().top,
-      },
-      900,
-      function () {
-        // Add hash to URL after scroll
-        window.location.hash = hash;
-      }
-    );
-  }
+        // Animate smooth scroll
+        $("html, body").animate({
+                scrollTop: $(hash).offset().top,
+            },
+            900,
+            function() {
+                // Add hash to URL after scroll
+                window.location.hash = hash;
+            }
+        );
+    }
 });
+
+// // Modal
+
+// let modal = document.getElementById("modalPics1");
+// let modalBtn = document.getElementById("modal1");
+// let closeBtn = document.getElementById("closeBtn");
+
+// modalBtn.addEventListener("click", openModal);
+// closeBtn.addEventListener("click", closeModal);
+// window.addEventListener('click', clickOutside)
+
+// function openModal() {
+//     modal.style.display = "block";
+// }
+
+// function closeModal() {
+//     modal.style.display = 'none'
+// }
+
+// function clickOutside(e) {
+//     if (e.target == modal) {
+//         modal.style.display = 'none'
+//     }
+// }
---
+++ Javascript/scrollanim.js
@@ -0,0 +1,70 @@
+// Video animation
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text1 = intro.querySelector(".note1");
+const text2 = intro.querySelector(".note2");
+const text3 = intro.querySelector(".note3");
+const text4 = intro.querySelector(".note4");
+const text5 = intro.querySelector(".note5");
+const mouse = document.getElementById("mousedown");
+
+//ScrollMagic
+const controller = new ScrollMagic.Controller();
+const controller1 = new ScrollMagic.Controller();
+
+// Scenes
+let scene = new ScrollMagic.Scene({
+        duration: 13000,
+        triggerElement: intro,
+        triggerHook: 0,
+    })
+    .setPin(intro)
+    .addTo(controller);
+
+// TextAnimation
+
+var wipeAnimation = new TimelineMax()
+    .fromTo(text1, 2, { opacity: 1 }, { opacity: 0 })
+    .fromTo(text2, 2, { opacity: 0 }, { opacity: 1 })
+    .to(text2, 1, { opacity: 0 })
+    .fromTo(text3, 2, { opacity: 0 }, { opacity: 1 })
+    .to(text3, 2, { opacity: 0 })
+    .fromTo(text4, 2, { opacity: 0 }, { opacity: 1 })
+    .to(text4, 2, { opacity: 0 })
+    .fromTo(text5, 2, { opacity: 0 }, { opacity: 1 });
+// .to(text5, 1, { opacity: 0 });
+
+new ScrollMagic.Scene({
+        triggerElement: intro,
+        triggerHook: "onLeave",
+        duration: 12000,
+    })
+    .setPin(scene)
+    .setTween(wipeAnimation)
+    .addTo(controller1);
+
+// Animation
+let accelamount = 0.1;
+let scrollposition = 0;
+let delay = 0;
+
+scene.on("update", (e) => {
+    scrollposition = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+    delay += (scrollposition - delay) * accelamount;
+    video.currentTime = delay;
+}, 90);
+
+window.addEventListener("scroll", () => {
+    var y = window.scrollY;
+    if (y >= 1200) {
+        mouse.classList.add('mouseDisappear');
+        mouse.style.opacity = 0;
+        return;
+    } else {
+        mouse.classList.remove('mouseDisappear');
+        mouse.style.opacity = 1;
+    }
+})
